3121401d62fca43e5397f92ae6a93439b24c411d
JamesOsborn-SE <3580335+JamesOsborn-SE@users.noreply.github.com>
2022-05-30 12:04:47 -0700

    added missing strings

--- app/src/main/java/com/example/eatpoopyoucat/MainActivity.kt
+++ app/src/main/java/com/example/eatpoopyoucat/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     private var connectionsClient: ConnectionsClient? = null
 
     // CodeName will be a hash of the sentence that starts the game
-    private val codeName: String = "New Game"
+    private val codeName: String = getString(R.string.new_game)
 
     private var requiredPermissions = when {
         Build.VERSION.SDK_INT >= VERSION_CODES.S -> {
