151152d22ee4078f7d30905231afe47d40862da9
JamesOsborn-SE <3580335+JamesOsborn-SE@users.noreply.github.com>
2022-06-12 17:39:42 -0700

    poc for drawing and saving nickname/id

--- app/src/main/java/dev/develsinthedetails/eatpoopyoucat/GameFragment.kt
+++ app/src/main/java/dev/develsinthedetails/eatpoopyoucat/GameFragment.kt
@@ -1,15 +1,23 @@
 package dev.develsinthedetails.eatpoopyoucat
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import dev.develsinthedetails.eatpoopyoucat.data.Game
+import dagger.hilt.android.AndroidEntryPoint
 import dev.develsinthedetails.eatpoopyoucat.databinding.FragmentGameBinding
+import dev.develsinthedetails.eatpoopyoucat.utilities.Gzip
+import kotlinx.android.synthetic.main.fragment_game.*
+import kotlinx.android.synthetic.main.fragment_game.view.*
+import org.apache.commons.codec.binary.Base64
+import java.io.ByteArrayOutputStream
 
+@AndroidEntryPoint
 class GameFragment : Fragment() {
 
     companion object {
@@ -28,18 +36,35 @@ class GameFragment : Fragment() {
             R.layout.fragment_game,
             container,
             false)
+
         return binding.root
     }
 
-       fun interface Callback {
-            fun add(game: Game?)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_save.setOnClickListener {
+            val bitmap= view.draw_view.getBitmap()
+            var outputSteam = ByteArrayOutputStream(bitmap.byteCount)
+            val x = bitmap.compress(Bitmap.CompressFormat.PNG, 10, outputSteam)
+            val gzip = Gzip()
+            val pngGZipped = gzip.compress(outputSteam.toByteArray())
+            var b64encode = Base64.encodeBase64(pngGZipped)
+            Log.d("PngGzipBase64Encoded",String(b64encode))
+            Log.d("PngGzipBase64Encoded","bitMap Bytes ${bitmap.byteCount}")
+            Log.d("PngGzipBase64Encoded","png Bytes ${outputSteam.toByteArray().size}")
+            Log.d("PngGzipBase64Encoded","gziped png Bytes ${pngGZipped.size}")
         }
-//    override fun onActivityCreated(savedInstanceState: Bundle?) {
-//        super.onActivityCreated(savedInstanceState)
-//        gameViewModel = ViewModelProvider(this).get(GameViewModel::class.java)
-//        gameViewModel.addGame()
-//        // TODO: Use the ViewModel
-//
-//    }
 
+        btn_erase.setOnClickListener {
+            view.draw_view.setErase()
+        }
+
+        btn_draw.setOnClickListener {
+            view.draw_view.setPen()
+        }
+
+        btn_clearAll.setOnClickListener {
+            view.draw_view.clearCanvas()
+        }
+    }
 }
--- app/src/main/java/dev/develsinthedetails/eatpoopyoucat/MainActivity.kt
+++ app/src/main/java/dev/develsinthedetails/eatpoopyoucat/MainActivity.kt
@@ -1,15 +1,22 @@
 package dev.develsinthedetails.eatpoopyoucat
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.nearby.connection.*
 import com.google.android.gms.nearby.connection.PayloadTransferUpdate.Status
 import dagger.hilt.android.AndroidEntryPoint
 import dev.develsinthedetails.eatpoopyoucat.permissionsUtiliy.PermissionsUtility
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var nickname: String
     private val tag = "EatPoopYouCat"
 
     // Our handle to Nearby Connections
@@ -17,23 +24,71 @@ class MainActivity : AppCompatActivity() {
 
     // CodeName will be a hash of the sentence that starts the game
     private lateinit var codeName: String
+    lateinit var shared : SharedPreferences
 
     private val strategy = Strategy.P2P_STAR
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        shared = getSharedPreferences("player", Context.MODE_PRIVATE)
+        nickname = shared.getString("nickname", "No name")!!
+        showNickname()
+
+        if(nickname !== "No name")
+            editNickname.setText(nickname)
+
+        if(shared.getString("playerId","") === "")
+            setPlayerId()
+
+        save.setOnClickListener{
+            setNickname()
+        }
+
+        editNickname.setOnEditorActionListener { _, actionId, _ ->
+            if(actionId == EditorInfo.IME_ACTION_NEXT || actionId == EditorInfo.IME_ACTION_DONE){
+                setNickname()
+                true
+            } else {
+                false
+            }
+        }
+
+        shared.registerOnSharedPreferenceChangeListener { sharedPreferences, key ->
+            if(key === "nickname")
+                txt.text = getString(R.string.welcome_message,
+                  sharedPreferences.getString("nickname" , "No name"))
+        }
+
+    }
+
+    private fun showNickname() {
+        txt.text = getString(R.string.welcome_message, nickname)
+    }
+
+    private fun setNickname() {
+        val edit = shared.edit()
+        nickname = editNickname.text.toString()
+        edit.putString("nickname", nickname)
+        edit.apply()
+        showNickname()
+    }
+
+    private fun setPlayerId() {
+        val edit = shared.edit()
+        edit.putString("playerId", UUID.randomUUID().toString())
+        edit.apply()
     }
 
     override fun onStart() {
         PermissionsUtility.requestPermissions(this)
         codeName = getString(R.string.new_game)
         super.onStart()
-
-        startAdvertising()
-        startDiscovery()
-        setStatusText(getString(R.string.status_searching))
-        PermissionsUtility.requestBackGroundPermission(this)
+//        startAdvertising()
+//        startDiscovery()
+//        setStatusText(getString(R.string.status_searching))
+//        PermissionsUtility.requestBackGroundPermission(this)
     }
 
     /** Starts looking for other players using Nearby Connections.  */
