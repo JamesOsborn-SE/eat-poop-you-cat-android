07f03352968364287c9a0ae898f862410fc06fb7
JamesOsborn-SE <3580335+JamesOsborn-SE@users.noreply.github.com>
2022-06-20 08:48:23 -0700

    stubbed in other views

---
+++ app/src/main/java/dev/develsinthedetails/eatpoopyoucat/DashboardFragment.kt
@@ -0,0 +1,18 @@
+package dev.develsinthedetails.eatpoopyoucat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DashboardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+}
