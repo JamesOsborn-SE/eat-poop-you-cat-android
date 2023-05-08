97341d5d0a4ddd6764a588330692331b45323ad7
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-05-08 14:56:04 -0700

    removed cruft

--- app/src/main/java/dev/develsinthedetails/eatpoopyoucat/data/Coordinates.kt
+++ app/src/main/java/dev/develsinthedetails/eatpoopyoucat/data/Coordinates.kt
@@ -9,24 +9,6 @@ data class Coordinates(private var xValue: Float, private var yValue: Float) {
         get() = xValue
     val y: Float
         get() = yValue
-
-    constructor(
-        x: Float,
-        y: Float,
-        sourceResolution: Resolution,
-        destinationResolution: Resolution
-    ) : this(x, y) {
-        val xCoefficient: Float =
-            (sourceResolution.width).toFloat() / (destinationResolution.width).toFloat()
-
-        val yCoefficient: Float = if (sourceResolution.height == sourceResolution.width)
-            xCoefficient
-        else
-            (sourceResolution.height).toFloat() / (destinationResolution.height).toFloat()
-
-        this.xValue = x / xCoefficient
-        this.yValue = y / yCoefficient
-    }
 }
 
 data class Resolution(val height: Int, val width: Int)
