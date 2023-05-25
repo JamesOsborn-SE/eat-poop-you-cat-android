dba116e38f625232004926ce0f01edb31a6effde
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-05-25 15:25:25 -0700

    removed unused code

--- app/src/main/java/dev/develsinthedetails/eatpoopyoucat/compose/home/HomeScreen.kt
+++ app/src/main/java/dev/develsinthedetails/eatpoopyoucat/compose/home/HomeScreen.kt
@@ -1,24 +1,19 @@
 package dev.develsinthedetails.eatpoopyoucat.compose.home
 
-import android.app.Activity
-import android.content.Context
-import android.content.ContextWrapper
 import android.content.res.Configuration
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,7 +21,6 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.develsinthedetails.eatpoopyoucat.R
 import dev.develsinthedetails.eatpoopyoucat.compose.Spinner
-import dev.develsinthedetails.eatpoopyoucat.permissionsUtiliy.PermissionsUtility
 import dev.develsinthedetails.eatpoopyoucat.ui.theme.AppTheme
 import dev.develsinthedetails.eatpoopyoucat.viewmodels.GreetingViewModel
 import java.util.UUID
@@ -39,9 +33,7 @@ fun HomeScreen(
     onNavigateToPreviousGames: () -> Unit,
 ) {
     HomeScreen(
-        userName = viewModel.userName,
         isLoading = viewModel.isLoading,
-        onNickNameValueChange = { viewModel.updateNickName(it) },
         onStartGame = {
             val entryId = UUID.randomUUID()
             viewModel.saveNewGame(
@@ -51,31 +43,19 @@ fun HomeScreen(
         onNavigateToPreviousGames = onNavigateToPreviousGames,
     )
 }
-internal fun Context.findActivity(): Activity {
-    var context = this
-    while (context is ContextWrapper) {
-        if (context is Activity) return context
-        context = context.baseContext
-    }
-    throw IllegalStateException("Permissions should be called in the context of an Activity")
-}
+
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
-    userName: String,
     isLoading: Boolean,
-    onNickNameValueChange: (String) -> Unit,
     onStartGame: () -> Unit,
     onNavigateToPreviousGames: () -> Unit,
 ) {
-    val context = LocalContext.current
-    val activity = context.findActivity()
-    PermissionsUtility.requestBackGroundPermission(activity)
     val padding = 20.dp
     AppTheme {
         // A surface container using the 'background' color from the theme
         Surface(
-            modifier = Modifier.fillMaxSize()
+            modifier = modifier.fillMaxSize()
                 .verticalScroll(ScrollState(0)),
             color = MaterialTheme.colorScheme.background
         ) {
@@ -93,25 +73,6 @@ fun HomeScreen(
                         modifier = defaultModifier,
                         fontSize = 18.sp
                     )
-                    if (0 == 1) {
-                        OutlinedTextField(
-                            value = userName,
-                            onValueChange = {
-                                onNickNameValueChange(it)
-                            },
-                            modifier = defaultModifier,
-                            enabled = true,
-                            readOnly = false,
-                            shape = RoundedCornerShape(8.dp),
-
-                            label = {
-                                Text(
-                                    stringResource(R.string.nickname),
-                                    modifier = modifier
-                                )
-                            },
-                        )
-                    }
                     StartGame(defaultModifier, onStartGame)
                     ViewPreviousGames(defaultModifier, onNavigateToPreviousGames)
                     Text(text = stringResource(id = R.string.app_description), modifier = defaultModifier)
@@ -125,6 +86,7 @@ fun HomeScreen(
 fun ViewPreviousGames(modifier: Modifier, navTo: () -> Unit) {
     Button(
         modifier=modifier,
+        colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.onSecondaryContainer),
         onClick = {
             navTo()
         }) {
@@ -143,29 +105,19 @@ fun StartGame(
     }
 }
 
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen(
+        isLoading = false,
+        onStartGame = {},
+        onNavigateToPreviousGames = {})
+}
 
 @Preview(
     uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
 )
 @Composable
 fun HomeScreenDarkPreview() {
-    HomeScreen(
-        userName = "Me",
-        isLoading = false,
-        onNickNameValueChange = {},
-        onStartGame = {},
-        onNavigateToPreviousGames = {})
-}
-
-
-
-@Preview
-@Composable
-fun HomeScreenPreview() {
-    HomeScreen(
-        userName = "Me",
-        isLoading = false,
-        onNickNameValueChange = {},
-        onStartGame = {},
-        onNavigateToPreviousGames = {})
+    HomeScreenPreview()
 }
