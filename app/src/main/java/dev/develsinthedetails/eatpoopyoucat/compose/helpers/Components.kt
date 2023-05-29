31f4db78d1f62984c4d8a05c2e46d4a6b4148837
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-05-29 13:36:05 -0700

    Add conformation dialog

--- app/src/main/java/dev/develsinthedetails/eatpoopyoucat/compose/helpers/Components.kt
+++ app/src/main/java/dev/develsinthedetails/eatpoopyoucat/compose/helpers/Components.kt
@@ -2,19 +2,28 @@ package dev.develsinthedetails.eatpoopyoucat.compose.helpers
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -44,7 +53,7 @@ fun Square(contents: @Composable () -> Unit) {
             Modifier.fillMaxWidth()
         }
     }
-    Box(modifier = fill){
+    Box(modifier = fill) {
         contents()
     }
 }
@@ -93,14 +102,22 @@ fun EndGameButton(
     modifier: Modifier = Modifier,
     onEnd: () -> Unit,
 ) {
+    var showEndGameConfirm by remember { mutableStateOf(false) }
     Button(
         modifier = modifier
             .padding(top = 15.dp),
         colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.onSecondaryContainer),
-        onClick = onEnd
+        onClick = { showEndGameConfirm = true }
     )
     {
         Text(stringResource(R.string.end_game_for_all))
+    }
+    if (showEndGameConfirm) {
+        ConfirmDialog(
+            onDismiss = { showEndGameConfirm = false },
+            onConfirm = onEnd,
+            action = stringResource(R.string.end_game_for_all)
+        )
     }
 }
 
@@ -112,4 +129,42 @@ fun ErrorText(isError: Boolean, textToDisplay: String) {
             text = textToDisplay,
             color = MaterialTheme.colorScheme.error,
         )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ConfirmDialog(
+    action: String,
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    AlertDialog(
+        modifier = Modifier.fillMaxWidth(),
+        onDismissRequest = { onDismiss() },
+        properties = DialogProperties(),
+        content = {
+            Column(
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.tertiaryContainer)
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(text = stringResource(R.string.are_you_sure, action))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Button(onClick = onConfirm) {
+                        Text(stringResource(R.string.yes))
+                    }
+                    Button(onClick = onDismiss) {
+                        Text(stringResource(R.string.no))
+                    }
+                }
+
+            }
+        }
+    )
 }
