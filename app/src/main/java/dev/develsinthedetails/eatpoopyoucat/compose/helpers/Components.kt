c35b291309597b5e458fbf74d82769501fcd2138
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-06-28 21:44:28 -0700

    Add icons to buttons and add previews

--- app/src/main/java/dev/develsinthedetails/eatpoopyoucat/compose/helpers/Components.kt
+++ app/src/main/java/dev/develsinthedetails/eatpoopyoucat/compose/helpers/Components.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,6 +15,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +27,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,24 +53,25 @@ fun Spinner(
     }
 }
 
-@Preview
-@Composable
-fun PreviewSpinner() {
-    Spinner()
-}
-
 @Composable
 fun SubmitButton(
     modifier: Modifier = Modifier,
     onSubmit: () -> Unit,
 ) {
-    Button(
-        modifier = modifier
-            .shadow(8.dp, shape = RoundedCornerShape(50.dp)),
-        onClick = onSubmit
-    ) {
-        Text(stringResource(R.string.submit))
-    }
+        Button(
+            modifier = modifier
+                .shadow(8.dp, shape = RoundedCornerShape(50.dp)),
+            onClick = onSubmit
+        ) {
+
+            Text(stringResource(R.string.submit))
+            Spacer(modifier = Modifier.size(5.dp))
+            Icon(
+                contentDescription = stringResource(id = R.string.submit),
+                painter = painterResource(id = R.drawable.ic_send_24),
+            )
+        }
+
 }
 
 @Composable
@@ -84,6 +88,11 @@ fun EndGameButton(
     )
     {
         Text(stringResource(R.string.end_game_for_all))
+        Spacer(modifier = Modifier.size(5.dp))
+        Icon(
+            contentDescription = stringResource(id = R.string.end_game_for_all),
+            painter = painterResource(id = R.drawable.ic_cancel_24),
+        )
     }
     if (showEndGameConfirm) {
         ConfirmDialog(
@@ -131,13 +140,51 @@ fun ConfirmDialog(
                 ) {
                     Button(onClick = onConfirm) {
                         Text(stringResource(R.string.yes))
+                        Spacer(modifier = Modifier.size(5.dp))
+                        Icon(
+                            contentDescription = stringResource(id = R.string.end_game_for_all),
+                            painter = painterResource(id = R.drawable.ic_check_24),
+                        )
                     }
                     Button(onClick = onDismiss) {
                         Text(stringResource(R.string.no))
+                        Spacer(modifier = Modifier.size(5.dp))
+                        Icon(
+                            contentDescription = stringResource(id = R.string.end_game_for_all),
+                            painter = painterResource(id = R.drawable.ic_cancel_24),
+                        )
                     }
                 }
 
             }
         }
+    )
+}
+
+
+@Preview
+@Composable
+fun PreviewSubmit() {
+    SubmitButton(onSubmit = {})
+}
+@Preview
+@Composable
+fun PreviewEndGame() {
+    EndGameButton(onEnd = {})
+}
+
+@Preview
+@Composable
+fun PreviewSpinner() {
+    Spinner()
+}
+
+@Preview
+@Composable
+fun PreviewConfirmDialog() {
+    ConfirmDialog(
+        action = "Oof",
+        onDismiss = {},
+        onConfirm = {},
     )
 }
