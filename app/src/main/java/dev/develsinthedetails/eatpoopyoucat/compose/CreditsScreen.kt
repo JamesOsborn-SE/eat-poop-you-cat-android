b76fda54f967c8c2cafbb82713f03ff03ca0d9fb
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2024-04-03 17:06:26 -0700

    chore: move credits and privacy policy screens to their own files

---
+++ app/src/main/java/dev/develsinthedetails/eatpoopyoucat/compose/CreditsScreen.kt
@@ -0,0 +1,91 @@
+package dev.develsinthedetails.eatpoopyoucat.compose
+
+import android.content.res.Configuration
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.develsinthedetails.eatpoopyoucat.BuildConfig
+import dev.develsinthedetails.eatpoopyoucat.R
+import dev.develsinthedetails.eatpoopyoucat.ui.theme.AppTheme
+import dev.develsinthedetails.eatpoopyoucat.utilities.ReadMetadata
+
+@Composable
+fun CreditsScreen() {
+    val uriHandler = LocalUriHandler.current
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(ScrollState(0)),
+        color = (MaterialTheme.colorScheme.background)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+                .background(MaterialTheme.colorScheme.background),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row {
+                Text(
+                    modifier = Modifier
+                        .padding(8.dp),
+                    text = ReadMetadata(LocalContext.current).getFullDescription()
+                )
+            }
+
+            Row {
+                TextButton(onClick = {
+                    uriHandler.openUri("https://hosted.weblate.org/engage/eat-poop-you-cat-android/")
+                }) {
+                    Text(
+                        text =
+                        stringResource(id = R.string.translations_welcome)
+                    )
+                }
+                Row {
+                    TextButton(onClick = {
+                        uriHandler.openUri("https://github.com/JamesOsborn-SE/eat-poop-you-cat-android/issues")
+                    }) {
+                        Text(
+                            text =
+                            stringResource(id = R.string.issues)
+                        )
+                    }
+                }
+            }
+            Text(text = stringResource(R.string.version_label, BuildConfig.VERSION_NAME))
+            Text(text = stringResource(R.string.git_hash_label, stringResource(R.string.git_hash)))
+            if (BuildConfig.DEBUG)
+                Text(text = stringResource(R.string.debugging_on_label, BuildConfig.DEBUG))
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(device = "spec:parent=Nexus 7 2013,orientation=landscape")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    device = "spec:parent=Nexus 7 2013,orientation=landscape"
+)
+@Composable
+fun PreviewCreditsScreen() {
+    AppTheme {
+        CreditsScreen()
+    }
+}
--- app/src/main/java/dev/develsinthedetails/eatpoopyoucat/compose/EatPoopYouCatApp.kt
+++ app/src/main/java/dev/develsinthedetails/eatpoopyoucat/compose/EatPoopYouCatApp.kt
@@ -1,33 +1,14 @@
 package dev.develsinthedetails.eatpoopyoucat.compose
 
 import android.content.res.Configuration
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import dev.develsinthedetails.eatpoopyoucat.BuildConfig
-import dev.develsinthedetails.eatpoopyoucat.R
 import dev.develsinthedetails.eatpoopyoucat.compose.draw.DrawScreen
 import dev.develsinthedetails.eatpoopyoucat.compose.home.HomeScreen
 import dev.develsinthedetails.eatpoopyoucat.compose.previousgames.PreviousGameScreen
@@ -38,7 +19,6 @@ import dev.develsinthedetails.eatpoopyoucat.data.EntryType
 import dev.develsinthedetails.eatpoopyoucat.data.type
 import dev.develsinthedetails.eatpoopyoucat.ui.theme.AppTheme
 import dev.develsinthedetails.eatpoopyoucat.utilities.ID
-import dev.develsinthedetails.eatpoopyoucat.utilities.ReadMetadata
 import dev.develsinthedetails.eatpoopyoucat.utilities.Screen
 
 @Composable
@@ -133,105 +113,3 @@ private fun navigateToNextTurn(navController: NavHostController): (Entry) -> Uni
             navController.navigate(Screen.Draw.byId(it.id.toString()))
     }
 
-@Composable
-fun CreditsScreen() {
-    val uriHandler = LocalUriHandler.current
-
-    Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(ScrollState(0)),
-        color = (MaterialTheme.colorScheme.background)
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(8.dp)
-                .background(MaterialTheme.colorScheme.background),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Row {
-                Text(
-                    modifier = Modifier
-                        .padding(8.dp),
-                    text = ReadMetadata(LocalContext.current).getFullDescription()
-                )
-            }
-
-            Row {
-                TextButton(onClick = {
-                    uriHandler.openUri("https://hosted.weblate.org/engage/eat-poop-you-cat-android/")
-                }) {
-                    Text(
-                        text =
-                        stringResource(id = R.string.translations_welcome)
-                    )
-                }
-                Row {
-                    TextButton(onClick = {
-                        uriHandler.openUri("https://github.com/JamesOsborn-SE/eat-poop-you-cat-android/issues")
-                    }) {
-                        Text(
-                            text =
-                            stringResource(id = R.string.issues)
-                        )
-                    }
-                }
-            }
-            Text(text = stringResource(R.string.version_label, BuildConfig.VERSION_NAME))
-            Text(text = stringResource(R.string.git_hash_label, stringResource(R.string.git_hash)))
-            if(BuildConfig.DEBUG)
-                Text(text = stringResource(R.string.debugging_on_label, BuildConfig.DEBUG))
-        }
-    }
-}
-
-@Composable
-fun PrivacyPolicyScreen() {
-    Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(ScrollState(0)),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(8.dp)
-                .background(MaterialTheme.colorScheme.background),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                modifier = Modifier
-                    .padding(8.dp),
-                text = ReadMetadata(LocalContext.current).getPrivacyPolicy()
-            )
-        }
-    }
-}
-
-@Preview
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Preview(device = "spec:parent=Nexus 7 2013,orientation=landscape")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    device = "spec:parent=Nexus 7 2013,orientation=landscape"
-)
-@Composable
-fun PreviewCreditsScreen() {
-    AppTheme {
-        CreditsScreen()
-    }
-}
-
-@Preview
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Preview(device = "spec:parent=Nexus 7 2013,orientation=landscape")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    device = "spec:parent=Nexus 7 2013,orientation=landscape"
-)
-@Composable
-fun PreviewPrivacyPolicyScreen() {
-    AppTheme {
-        PrivacyPolicyScreen()
-    }
-}
