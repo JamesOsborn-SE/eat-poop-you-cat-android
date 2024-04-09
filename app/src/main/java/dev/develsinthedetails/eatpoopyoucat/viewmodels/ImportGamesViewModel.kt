5143efe11f3fc5efd774f506256ecc2e3ad5eebd
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2024-04-09 09:15:09 -0700

    with async await launch isn't needed

--- app/src/main/java/dev/develsinthedetails/eatpoopyoucat/viewmodels/ImportGamesViewModel.kt
+++ app/src/main/java/dev/develsinthedetails/eatpoopyoucat/viewmodels/ImportGamesViewModel.kt
@@ -2,14 +2,12 @@ package dev.develsinthedetails.eatpoopyoucat.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.develsinthedetails.eatpoopyoucat.SharedPref
 import dev.develsinthedetails.eatpoopyoucat.data.AppRepository
 import dev.develsinthedetails.eatpoopyoucat.data.Entry
 import dev.develsinthedetails.eatpoopyoucat.data.GameWithEntries
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,25 +23,22 @@ class ImportGamesViewModel @Inject constructor(
     private var _numberOfEntriesAdded: MutableStateFlow<Int> = MutableStateFlow(0)
     val numberOfEntriesAdded = _numberOfEntriesAdded.asLiveData()
 
-    private fun addGame(gameWithEntries: GameWithEntries) {
-        viewModelScope.launch {
-            repository.createGame(gameWithEntries.game)
-            addEntries(gameWithEntries.entries)
-            _numberOfGamesAdded.emit(++_numberOfGamesAdded.value)
-        }
+    private suspend fun addGame(gameWithEntries: GameWithEntries) {
+        repository.createGame(gameWithEntries.game)
+        addEntries(gameWithEntries.entries)
+        _numberOfGamesAdded.emit(++_numberOfGamesAdded.value)
     }
-    private fun addEntries(entries: List<Entry>) {
-        viewModelScope.launch {
-            entries.forEach {
-                val newEntry = it.copy(playerId = SharedPref.playerId())
-                repository.createEntry(newEntry)
-                _numberOfEntriesAdded.emit(++_numberOfEntriesAdded.value)
-            }
+
+    private suspend fun addEntries(entries: List<Entry>) {
+        entries.forEach {
+            val newEntry = it.copy(playerId = SharedPref.playerId())
+            repository.createEntry(newEntry)
+            _numberOfEntriesAdded.emit(++_numberOfEntriesAdded.value)
         }
     }
 
     suspend fun addGames(gamesToImport: List<GameWithEntries>, closeStream: () -> Unit) {
-        val existingGameIds = repository.getAllGames().map { g->g.id }
+        val existingGameIds = repository.getAllGames().map { g -> g.id }
         gamesToImport.forEach {
             if (!existingGameIds.any { g -> g == it.game.id })
                 addGame(it)
@@ -51,7 +46,7 @@ class ImportGamesViewModel @Inject constructor(
                 val existingEntries = repository.getEntriesAsync(it.game.id.toString())
                 val missingEntries =
                     it.entries.filter { e -> !existingEntries.any { ee -> ee.id == e.id } }
-                if(missingEntries.any())
+                if (missingEntries.any())
                     addEntries(missingEntries)
             }
         }
