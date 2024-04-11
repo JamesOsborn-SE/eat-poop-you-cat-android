05fefacd0a9969d20635f4fbdada4ae8a58db3e2
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2024-04-11 10:48:43 -0700

    Chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 16
-        versionName = "1.3.1"
+        versionName = "1.3.2"
         testInstrumentationRunner = "dev.develsinthedetails.eatpoopyoucat.utilities.MainTestRunner"
         vectorDrawables {
             useSupportLibrary = true
