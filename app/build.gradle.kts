6fdce44d08d7e46812d76c95f214f86ffa2fddf0
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2024-04-15 20:04:11 -0700

    chore: bump version
    Signed-off-by: James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.develsinthedetails.eatpoopyoucat"
         minSdk = 21
         targetSdk = 34
-        versionCode = 19
-        versionName = "1.4.2"
+        versionCode = 20
+        versionName = "1.4.3"
         testInstrumentationRunner = "dev.develsinthedetails.eatpoopyoucat.utilities.MainTestRunner"
         vectorDrawables {
             useSupportLibrary = true
