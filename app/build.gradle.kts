4cd1304f0bb3d32d7a4c580939e351010400725a
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2024-07-22 14:53:59 -0700

    version bump
    Signed-off-by: James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.develsinthedetails.eatpoopyoucat"
         minSdk = 21
         targetSdk = 34
-        versionCode = 22
-        versionName = "1.4.5"
+        versionCode = 23
+        versionName = "1.4.6"
         testInstrumentationRunner = "dev.develsinthedetails.eatpoopyoucat.utilities.MainTestRunner"
         vectorDrawables {
             useSupportLibrary = true
