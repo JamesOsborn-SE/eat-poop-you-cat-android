8b21b8e431e7c7f5b18a83d71f6e2c49fc20e88e
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2026-07-30 09:59:49 -0700

    clean up and stubs
    Signed-off-by: James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>

---
+++ app/src/main/java/dev/develsinthedetails/eatpoopyoucat/NetGameActivity.kt
@@ -0,0 +1,6 @@
+package dev.develsinthedetails.eatpoopyoucat
+
+import android.app.Activity
+
+class NetGameActivity: Activity() {
+}
---
+++ app/src/main/java/dev/develsinthedetails/eatpoopyoucat/ui/screens/LanGame.kt
@@ -0,0 +1,9 @@
+package dev.develsinthedetails.eatpoopyoucat.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun LanGameScreen(nav: NavHostController) {
+    TODO("Not yet implemented")
+}
